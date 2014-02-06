2749b716cfd03aa4ce150a0020976b9247268586
Jan Krag <jankrag@gmail.com>
2014-02-06 21:13:37 +0100

    initialised Angular project using yeoman

---
+++ Pedigraph/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pedigraphApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
