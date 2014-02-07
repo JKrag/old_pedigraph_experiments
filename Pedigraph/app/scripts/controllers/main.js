fad076c94c6e1dac56b01ec504d235ce58e61866
Jan Krag <jankrag@gmail.com>
2014-02-07 21:32:03 +0100

    Just for fun, I now return 2 nodes (the matched cat and its sire) in the search result

--- Pedigraph/app/scripts/controllers/main.js
+++ Pedigraph/app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('pedigraphApp')
   .controller('MainCtrl', function($scope, $http) {
     $scope.doSearch = function() {
       $http.post('http://localhost:7474/db/data/cypher/',
-        '{"query" : "MATCH (a:Cat) WHERE a.id=~{id} RETURN a LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
+        '{"query" : "MATCH (a:Cat)-[:SIRE]->(s:Cat) WHERE a.id=~{id} RETURN a,s LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
         .success(function(data) {
           $scope.cats = data.data;
           console.log(data.data[0][0].data.id);
