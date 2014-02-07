952e573b653febc1ef3b24b382c310661a221855
Jan Krag <jankrag@gmail.com>
2014-02-07 18:27:19 +0100

    Moved header and footer from main view to index.html. Added my own search page stuff to main Added first implementation of Neo4J search in main.js

--- Pedigraph/app/scripts/controllers/main.js
+++ Pedigraph/app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('pedigraphApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($scope, $http) {
+    $scope.doSearch = function() {
+      $http.post('http://localhost:7474/db/data/cypher/',
+        '{"query" : "MATCH (a:Cat) WHERE a.id=~{id} return a", "params" : {"id" : ".*' + $scope.search + '.*"} }')
+        .success(function(data) {
+          $scope.cats = data.data;
+          console.log(data.data[0][0].data.id);
+        })
+        .error(function() {
+          console.log('Failed!');
+        });
+    };
   });
