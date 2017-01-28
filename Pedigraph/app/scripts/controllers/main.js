1b87706593036445e41561752949ff2929429ceb
Jan Krag <jankrag@gmail.com>
2017-01-28 12:27:09 +0100

    Some 3 year old changes that were not commited.
    Probably want to save these before cleaning old computer

--- Pedigraph/app/scripts/controllers/main.js
+++ Pedigraph/app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('pedigraphApp')
   .controller('MainCtrl', function($scope, $http) {
     $scope.doSearch = function() {
       $http.post('http://localhost:7474/db/data/cypher/',
-        '{"query" : "MATCH (a:Cat)-[:SIRE]->(s:Cat) WHERE a.id=~{id} RETURN a,s LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
+        '{"query" : "MATCH (a:Cat) OPTIONAL MATCH (a:Cat)-[:SIRE]->(s:Cat) OPTIONAL MATCH (a:Cat)-[:DAM]->(d:Cat) WHERE a.id=~{id} RETURN a,s LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
         .success(function(data) {
           $scope.cats = data.data;
           console.log(data.data[0][0].data.id);
@@ -14,3 +14,8 @@ angular.module('pedigraphApp')
         });
     };
   });
+
+//    '{"query" : "MATCH (a:Cat) OPTIONAL MATCH (a:Cat)-[:SIRE]->(s:Cat) OPTIONAL MATCH (a:Cat)-[:DAM]->(d:Cat) WHERE a.id=~{id} RETURN a,s LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
+//    '{"query" : "MATCH (d:Cat)<-[:DAM]-(a:Cat)-[:SIRE]->(s:Cat)  WHERE a.id=~{id} RETURN a,s,d LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
+//    '{"query" : "MATCH (a:Cat) WHERE a.id=~{id} OPTIONAL MATCH (a)-[rs:SIRE]->(s) OPTIONAL MATCH (a)-[rd:DAM]->(d) RETURN a,s,d LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
+//    '{"query" : "MATCH (a:Cat) OPTIONAL MATCH (a:Cat)-[:SIRE]->(s:Cat) OPTIONAL MATCH (a:Cat)-[:DAM]->(d:Cat) WHERE a.id=~{id} RETURN a,s LIMIT 100", "params" : {"id" : "' + $scope.search + '.*"} }')
